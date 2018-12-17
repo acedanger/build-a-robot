3c9e0cbdae6ded89a9ef9dd401f54ac09b94ec5e
Peter Wood <acedanger49@gmail.com>
2018-12-17 07:21:11 -0500

    prevent user from leaving a page if robot config'd but not added to cart

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -76,10 +76,20 @@ import CollapsibleSection from '../shared/CollapsibleSection.vue';
 
 export default {
   name: 'RobotBuilder',
+  beforeRouteLeave(to, from, next) {
+    if (this.addedToCart) {
+      next(true);
+    } else {
+      // eslint-disable-next-line no-alert
+      const response = confirm('You have not added your robot to your cart, are you sure you want to leave?');
+      next(response);
+    }
+  },
   components: { PartSelector, CollapsibleSection },
   data() {
     return {
       availableParts,
+      addedToCart: false,
       cart: [],
       selectedRobot: {
         head: {},
@@ -111,6 +121,7 @@ export default {
       const cost = robot.head.cost + robot.leftArm.cost
         + robot.torso.cost + robot.rightArm.cost + robot.base.cost;
       this.cart.push(Object.assign({}, robot, { cost }));
+      this.addedToCart = true;
     },
   },
 };
